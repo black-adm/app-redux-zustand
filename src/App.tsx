8e00cfa29ae15ddf1caa86060954e529c63ed3ac
black-adm <mattheuscontato17@gmail.com>
2023-11-13 12:57:47 -0300

    reset project and install tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Provider as ReduxProvider } from "react-redux";
-import { AddTask } from "./components/AddTask";
-import { TaskList } from "./components/TaskList";
 import { store } from "./store";
+import { Player } from "./pages/Player";
 
 export function App() {
 
   return (
     <ReduxProvider store={store}>
-      <TaskList />
-      <AddTask />
+      <Player />
     </ReduxProvider>
   )
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,28 +1,9 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { useSelector, TypedUseSelectorHook } from "react-redux";
 
-const taskSlice = createSlice({
-    name: 'task',
-    initialState: [
-        'Ir para faculdade',
-        'Estudar React',
-        'Levar a Alice no pediatra'
-    ],
-
-    reducers: {
-        add: (state, action) => {
-            state.push(action.payload.newTask)
-        },
-    },
-})
-
 export const store = configureStore({
-    reducer: {
-        task: taskSlice.reducer
-    }
+    reducer: {}
 })
-
-export const { add } = taskSlice.actions
 
 export type RootState = ReturnType<typeof store.getState>
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
