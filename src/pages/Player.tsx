7385e088114fcb5b672d984f653eddf760e0f947
black-adm <mattheuscontato17@gmail.com>
2023-11-13 13:20:13 -0300

    install phosphor-icons and style header

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,7 +1,30 @@
+import { ChatTeardropText } from "@phosphor-icons/react";
+
 export function Player() {
     return (
-        <div>
-            
+        <div className="bg-primary-gray h-screen flex justify-center items-center text-dark-primary">
+            <div className="flex w-[1100px] flex-col gap-6">
+                <div className="flex items-center justify-between px-2">
+
+                    <div className="flex flex-col gap-1">
+                        <h1 className="text-2xl font-bold">
+                            Fundamentos Redux
+                        </h1>
+                        <span className="text-sm text-light-primary">
+                            Módulo "Desvendando o Redux"
+                        </span>
+                    </div>
+
+                    <button className="px-3 py-2 text-sm text-primary-white font-medium flex items-center gap-2 rounded-sm bg-medium-primary hover:bg-light-primary">
+                        <ChatTeardropText className="w-4 h-4" />
+                        Deixar feedback
+                    </button>
+                </div>
+
+                <main>
+
+                </main>
+            </div>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ export default {
     './src/**/*.tsx'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-white': '#f8edeb',
+        'primary-gray': '#e3d5ca',
+        'dark-primary': '#14213d',
+        'medium-primary': '#22223b',
+        'light-primary': '#4a4e69'
+      }
+    },
   },
   plugins: [],
 }
