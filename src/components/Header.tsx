0aab1f621fc8e8ad3f5da03494b8438b8750523f
black-adm <mattheuscontato17@gmail.com>
2023-11-13 22:16:22 -0300

    highlighting active class

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,26 @@
 import { MonitorPlay, Stack } from "@phosphor-icons/react";
+import { useAppSelector } from "../store";
 
 export function Header() {
+    const { currentModule, currentLesson } = useAppSelector(state => {
+        const { currentModuleIndex, currentLessonIndex } = state.player
+
+        const currentModule = state.player.course.modules[currentModuleIndex]
+        const currentLesson = currentModule.lessons[currentLessonIndex]
+
+        return { currentModule, currentLesson }
+    })
+
     return (
         <div className="mt-2 flex flex-col gap-1">
             <h1 className="flex items-center gap-2 text-2xl font-bold">
                 <MonitorPlay className="mb-1 h-7 w-7" weight="bold" />
-                Fundamentos do Redux
+                {currentLesson.title}
             </h1>
 
             <span className="mt-2 flex items-center gap-1 text-sm font-medium tracking-wide text-light-primary">
                 <Stack className="mb-1 h-4 w-4" weight="bold" />
-                Módulo - "Desvendando o Redux"
+                Módulo - "{currentModule.title}"
             </span>
         </div>
     )
--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,18 +1,25 @@
-import { VideoCamera } from "@phosphor-icons/react";
+import { PlayPause, VideoCamera } from "@phosphor-icons/react";
 
 interface LessonProps {
     title: string
     duration: string
+    isCurrent?: boolean
     onPlay: () => void
 }
 
-export function Lesson({ title, duration, onPlay }: LessonProps) {
+export function Lesson({ title, duration, onPlay, isCurrent = false }: LessonProps) {
     return (
         <button
             onClick={onPlay}
-            className="flex items-center gap-3 text-sm text-primary-white"
+            data-active={isCurrent}
+            disabled={isCurrent}
+            className="flex items-center gap-3 text-sm text-primary-white data-[active=true]:text-indigo-300 enabled:hover:text-indigo-400"
         >
-            <VideoCamera className="w-4 h-4 text-primary-gray" weight="bold" />
+            {isCurrent ? (
+                <PlayPause className="w-4 h-4 text-indigo-300" weight="bold" />
+            ) : (
+                <VideoCamera className="w-4 h-4 text-primary-gray" weight="bold" />
+            )}
             <span>{title}</span>
             <span className="ml-auto font-mono text-xs text-primary-white">
                 {duration}
